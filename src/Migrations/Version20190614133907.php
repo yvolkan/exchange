4478fe0f1ee7d687397e562398fa77e797aea68f
Volkan YILMAZ <volkyilmaz@hotmail.com>
2019-06-15 03:37:00 +0200

    Fix: Migration name

--- src/Migrations/Version20190614133907.php
+++ src/Migrations/Version20190614133907.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20190613133907 extends AbstractMigration
+final class Version20190614133907 extends AbstractMigration
 {
     public function getDescription() : string
     {
